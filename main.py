c749e9dac2e0092c8164a2a86f3ed1076630877d
shaiful019 <shaiful019@gmail.com>
2019-04-06 01:58:17 +0600

    added pause button

--- main.py
+++ main.py
@@ -39,49 +39,70 @@ subMenu.add_command(label="About Us", command=aboutUs)
 mixer.init()  # Initializing the mixer
 
 root.title("mPlayer")
-root.geometry("600x480")
+root.geometry('400x400+200+190')
 
 text = Label(root, text='Let\'s make some noise')
 text.pack()
 
 
 def playMusic():
-    try:
-        mixer.music.load(filename)
-        mixer.music.play()
-        statusbar['text'] = "Playing "+ os.path.basename(filename)
+    global paused
+    if(paused):
+        mixer.music.unpause()
+        statusbar['text'] = "Playing " + os.path.basename('song1.mp3')
+    else:
+        try:
+            mixer.music.load('song1.mp3')
+            mixer.music.play()
+            statusbar['text'] = "Playing "+ os.path.basename('song1.mp3')
 
-    except:
-        tkinter.messagebox.showerror('File not found', "Please load a music file first.")
-
-
-playPhoto = PhotoImage(file='play.png')
-playBtn = Button(root, image=playPhoto, command=playMusic)
-playBtn.pack()
+        except:
+            tkinter.messagebox.showerror('File not found', "Please load a music file first.")
 
 
 def stopMusic():
+    global paused
     mixer.music.stop()
+    paused = FALSE
     statusbar['text'] = "Music stopped"
 
-
-stopPhoto = PhotoImage(file='stop.png')
-stopBtn = Button(root, image=stopPhoto, command=stopMusic)
-stopBtn.pack()
-
-
+paused = FALSE
+def pauseMusic():
+    global paused
+    paused = TRUE
+    mixer.music.pause()
+    statusbar['text'] = "Music Paused"
 
 def set_vol(val):
     volume = int(val) / 100
     mixer.music.set_volume(volume)
 
+#Adding play button
+
+playPhoto = PhotoImage(file='images/play.png')
+playBtn = Button(root, image=playPhoto, command=playMusic)
+playBtn.pack()
+
+#Adding stop button
+
+stopPhoto = PhotoImage(file='images/stop.png')
+stopBtn = Button(root, image=stopPhoto, command=stopMusic)
+stopBtn.pack()
+
+#Add pause button
+
+pausePhoto = PhotoImage(file='images/pause.png')
+pauseBtn = Button(root, image=pausePhoto, command=pauseMusic)
+pauseBtn.pack()
+
+#Adding volume level
 
 scale = Scale(root, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
 scale.set(50)
 scale.pack()
 
 # Adding satusbar
-statusbar = Label(root, text="Filename", relief=SUNKEN, anchor=W)
+statusbar = Label(root, text="***Welcome to the world of music***", relief=SUNKEN, anchor=W)
 statusbar.pack(side=BOTTOM, fill=X)
 
 root.mainloop()
